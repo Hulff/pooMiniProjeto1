99cb8a6a84203a75ada4c31577a4f0c27821b441
Hugo <ph2717693@gmail.com>
2024-08-20 15:13:00 -0300

    Atualizar o Campeonato.java

--- miniprojetoPOO1/src/miniprojetoPOO1/Campeonato.java
+++ miniprojetoPOO1/src/miniprojetoPOO1/Campeonato.java
@@ -4,21 +4,21 @@ import java.util.ArrayList;
 public class Campeonato {
     ArrayList<Clube> clubes = new ArrayList<Clube>();
 
-    public static void bubbleSort(ArrayList<Clube> array) {
-        int n = array.size();
+    public void bubbleSort() {
+        int n = clubes.size();
         for (int i = 0; i < n - 1; i++) {
             for (int j = 0; j < n - i - 1; j++) {
-                if (array.get(j).pontos < array.get(j + 1).pontos) {
-                    // Troca array[j] e array[j+1]
-                    Clube temp = array.get(j);
-                    array.set(j, array.get(j + 1));
-                    array.set(j + 1, temp);
-                } else if(array.get(j).pontos == array.get(j + 1).pontos) {
-                	if (array.get(j).saldoGols < array.get(j + 1).saldoGols) {
-                        // Troca array[j] e array[j+1]
-                        Clube temp = array.get(j);
-                        array.set(j, array.get(j + 1));
-                        array.set(j + 1, temp);
+                if (clubes.get(j).pontos < clubes.get(j + 1).pontos) {
+                    // Troca clubes[j] e clubes[j+1]
+                    Clube temp = clubes.get(j);
+                    clubes.set(j, clubes.get(j + 1));
+                    clubes.set(j + 1, temp);
+                } else if(clubes.get(j).pontos == clubes.get(j + 1).pontos) {
+                        if (clubes.get(j).saldoGols < clubes.get(j + 1).saldoGols) {
+                        // Troca clubes[j] e clubes[j+1]
+                        Clube temp = clubes.get(j);
+                        clubes.set(j, clubes.get(j + 1));
+                        clubes.set(j + 1, temp);
                     }
                 }
             }
@@ -26,18 +26,19 @@ public class Campeonato {
     }
 
     public void jogarCampeonato() {
-    	//jogos de ida
+            //jogos de ida
         for(int i=0;i<clubes.size();i++) {
-        	for(int j=i+1;j<clubes.size();j++) {
-        		jogarPartida(clubes.get(i),clubes.get(j));
-            }        	
+                for(int j=i+1;j<clubes.size();j++) {
+                        jogarPartida(clubes.get(i),clubes.get(j));
+            }                
         }
         //jogos de volta
         for(int i=0;i<clubes.size();i++) {
-        	for(int j=i;j<clubes.size();j++) {
-        		jogarPartida(clubes.get(i),clubes.get(j));
-            }        	
+                for(int j=i;j<clubes.size();j++) {
+                        jogarPartida(clubes.get(i),clubes.get(j));
+            }                
         }
+        bubbleSort();
     }
 
     private void jogarPartida(Clube m, Clube v) {
@@ -63,15 +64,15 @@ public class Campeonato {
     }
 
     public String getClassificacao() {
-        bubbleSort(clubes);
+        
         String classificacao = "|";
         for (int i = 0; i < clubes.size(); i++) {
             Clube clube = clubes.get(i);
-            String classLine = clube.nome + " - Pontos: " + clube.pontos+" - Saldo de Gols: "+clube.saldoGols;
+            String classLine =(i+1)+"° "+clube.nome + " - Pontos: " + clube.pontos;
             classificacao += classLine+"|";
             System.out.println(classLine);
         }
-        return classificacao.toString();
+        return classificacao;
     }
 
     public String getCampeao() {
